beff224e0d6b16b910ff2f0650ce0bc2550f2354
Clay Roberson <crobersoncollege@gmail.com>
2023-09-06 14:46:20 -0600

    added Clay's bio to About page

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -12,7 +12,7 @@ export const teamData =
 export const memberData = [
     {
         name: "Clay Roberson",
-        bio: "Bio Here",
+        bio: "My name is Clay Roberson. I'm currently a Junior at CSU studying Computer Science with a concentration on Machine Learning and Artificial Intelligence. My current goal is to move into the field of AI-Ops. The perfect combination of Dev-Ops and AI could bring efficiency to any work force. My biggest achievement is becoming the Editor in Chief of my National Award-winning High School yearbook.",
         homeTown: "Centennial, CO",
         imagePath: memberPic
     },
