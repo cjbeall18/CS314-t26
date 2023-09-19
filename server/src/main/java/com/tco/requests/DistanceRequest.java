ab6be73d659777aa159a29d5374e29437354061c
Benjamin Ayala <101277872+BiggBenDesign@users.noreply.github.com>
2023-09-19 11:41:59 -0600

    Update DistanceRequest.java
    added void return type to DistanceRequest()

--- server/src/main/java/com/tco/requests/DistanceRequest.java
+++ server/src/main/java/com/tco/requests/DistanceRequest.java
@@ -24,7 +24,7 @@ public class DistanceRequest extends Request {
 
     }
 
-    public DistanceRequestRequest() {
+    public void DistanceRequest() {
         this.requestType = "distances";
     }
 }
