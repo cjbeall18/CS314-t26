c597037b437af75fa06de14180e5522603c69d08
Evan Loy <loy.evan@yahoo.com>
2023-09-20 17:51:51 -0600

    Adds a method to find the total distance from an array of distances.

--- server/src/main/java/com/tco/requests/Distances.java
+++ server/src/main/java/com/tco/requests/Distances.java
@@ -1,6 +1,16 @@
 package com.tco.requests;
 import java.util.ArrayList;
 
-public class Distances extends ArrayList<String> {
+public class Distances extends ArrayList<Integer> {
     
+    public long total() {
+        
+        long totalDistance = 0;
+
+        for (Integer distance : this) {
+                totalDistance += distance;
+        }
+        
+        return totalDistance;
+    }    
 }
