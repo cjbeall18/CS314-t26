b2343b9839b70d8bec8b7ffa3eec94bf740bf152
Chris Beall <cjbeall18@hotmail.com>
2023-10-07 17:38:28 +0000

    changed logger in FindRequest frrom ConfigRequest.class to FindRequest.class

--- server/src/main/java/com/tco/requests/FindRequest.java
+++ server/src/main/java/com/tco/requests/FindRequest.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FindRequest extends Request {
-    private static final transient Logger log = LoggerFactory.getLogger(ConfigRequest.class);
+    private static final transient Logger log = LoggerFactory.getLogger(FindRequest.class);
 
     private Places placesList;
     private String match;
