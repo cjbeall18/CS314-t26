0b895d254d7feb2d9330a725f3b3865f73f32b8f
Benjamin Ayala <bigben.design@outlook.com>
2023-10-10 16:11:22 -0600

    Added Database.java skeleton and TestDatabase initial test

---
+++ server/src/main/java/com/tco/requests/Database.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.lang.Exception;
+import com.tco.requests.Places;
+import com.tco.requests.Distances;
+
+public class Database {
+
+	Database() {}
+
+}
