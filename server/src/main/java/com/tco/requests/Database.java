643846f6d587cc9a95421ade2c0699556f6b6167
Clay Roberson <crobersoncollege@gmail.com>
2023-10-10 17:08:55 -0600

    added convertQuertResultsToPlaces

--- server/src/main/java/com/tco/requests/Database.java
+++ server/src/main/java/com/tco/requests/Database.java
@@ -14,4 +14,18 @@ public class Database {
 
 	public Integer found() {return 0;}
 
+	private static Places convertQueryResultsToPlaces(ResultSet results, String columns) throws Exception {
+		int count = 0;
+		String[] cols = columns.split(",");
+		Places places = new Places();
+		while (results.next()) {
+			Place place = new Place();
+			for (String col: cols) {
+				place.put(col, results.getString(col));
+			}
+			place.put("index", String.format("%d",++count));
+			places.add(place);
+		}
+		return places;
+	}
 }
