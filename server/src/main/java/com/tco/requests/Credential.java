2fec06f8d9391ca52d49ee74302e649c18b8227c
Ethan Pitera <ejpitera@gmail.com>
2023-10-10 16:34:39 -0600

    added credential implementation

--- server/src/main/java/com/tco/requests/Credential.java
+++ server/src/main/java/com/tco/requests/Credential.java
@@ -11,11 +11,7 @@ class Credential {
 	final static String USER = "cs314-db";
 	final static String PASSWORD = "eiK5liet1uej";
 	// connection information when using port forwarding from localhost
-	//final static String URL = "jdbc:mariadb://127.0.0.1:56247/cs314";
-
-    
-
-    
+	
 	private String url() {
         String useTunnel = System.getenv("CS314_USE_DATABASE_TUNNEL");
         String onDocker = System.getenv("CS314_DOCKER");
