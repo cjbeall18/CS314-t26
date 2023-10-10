b626baf171b794fdc79a43bd4b60d0dfc89dbe77
Ethan Pitera <ejpitera@gmail.com>
2023-10-10 15:58:06 -0600

    added credential skeleton

---
+++ server/src/main/java/com/tco/requests/Credential.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.lang.Exception;
+
+class Credential {}
