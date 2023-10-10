52448ff7c6cd1623230d60fdc0be3b1ec88a5371
Ethan Pitera <ejpitera@gmail.com>
2023-10-10 16:22:40 -0600

    added credential implementation

--- server/src/main/java/com/tco/requests/Credential.java
+++ server/src/main/java/com/tco/requests/Credential.java
@@ -6,4 +6,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.lang.Exception;
 
-class Credential {}
+class Credential {
+    // shared user with read-only access
+	final static String USER = "cs314-db";
+	final static String PASSWORD = "eiK5liet1uej";
+	// connection information when using port forwarding from localhost
+	//final static String URL = "jdbc:mariadb://127.0.0.1:56247/cs314";
+
+    
+
+    
+	private String url() {
+        String useTunnel = System.getenv("CS314_USE_DATABASE_TUNNEL");
+        String onDocker = System.getenv("CS314_DOCKER");
+        String URL = "";
+
+        if(useTunnel != null && useTunnel.equals("true")) {
+            URL = "jdbc:mariadb://127.0.0.1:56247/cs314";
+        }
+        else if(onDocker != null && onDocker.equals("true")) {
+            URL = "jdbc:mariadb://127.0.0.1:3306/cs314";
+        }
+        else {
+            URL = "jdbc:mariadb://faure.cs.colostate.edu/cs314";
+        }
+		return URL;
+	}
+}
