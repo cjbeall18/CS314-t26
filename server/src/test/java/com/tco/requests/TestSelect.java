4777d41feff2f963f7f6ece485fd876742c1a041
Chris Beall <cjbeall18@hotmail.com>
2023-10-11 21:27:48 +0000

    added select test case

--- server/src/test/java/com/tco/requests/TestSelect.java
+++ server/src/test/java/com/tco/requests/TestSelect.java
@@ -2,11 +2,21 @@ package com.tco.requests;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestSelect {
-    
+    Select sel = new Select();
+
     @Test
     @DisplayName("cjbeall:")
-    public void testCase1() {
+    public void testMatch() {
+        assertEquals(sel.match("Chris", 15), "SELECT DISTINCT id,name,municipality,iso_region,iso_country,continent,latitude,longitude,altitude FROM world WHERE name LIKE \"%" + "Chris" + "%\" " + "LIMIT 15 ;"); 
+    }
+
+    @Test
+    @DisplayName("cjbeall:")
+    public void testFound() {
+        assertEquals( sel.found("Chris"), "SELECT COUNT(*) AS count  FROM world WHERE name LIKE \"%" + "Chris" + "%\"" + "  ;"); 
     }
 }
