b4fda1504a425fc250d5fa81e7aea8832d0ea99c
Chris Beall <cjbeall18@hotmail.com>
2023-10-11 21:59:51 +0000

    added message to test case display name

--- server/src/test/java/com/tco/requests/TestSelect.java
+++ server/src/test/java/com/tco/requests/TestSelect.java
@@ -9,13 +9,13 @@ public class TestSelect {
     Select sel = new Select();
 
     @Test
-    @DisplayName("cjbeall:")
+    @DisplayName("cjbeall: test match, chris limit 15")
     public void testMatch() {
         assertEquals(sel.match("Chris", 15), "SELECT DISTINCT id,name,municipality,iso_region,iso_country,continent,latitude,longitude,altitude FROM world WHERE name LIKE \"%" + "Chris" + "%\" " + "LIMIT 15 ;"); 
     }
 
     @Test
-    @DisplayName("cjbeall:")
+    @DisplayName("cjbeall: test match chris")
     public void testFound() {
         assertEquals( sel.found("Chris"), "SELECT COUNT(*) AS count  FROM world WHERE name LIKE \"%" + "Chris" + "%\"" + "  ;"); 
     }
