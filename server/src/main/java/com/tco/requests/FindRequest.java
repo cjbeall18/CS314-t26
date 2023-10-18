2fb679220ce1c45580800b7e6cb521f94440d5f9
Chris Beall <cjbeall18@hotmail.com>
2023-10-18 20:14:59 +0000

    changed all variable names from placesList to places

--- server/src/main/java/com/tco/requests/FindRequest.java
+++ server/src/main/java/com/tco/requests/FindRequest.java
@@ -27,7 +27,6 @@ public class FindRequest extends Request {
 
     @Override
     public void buildResponse() {
-        // placesList = buildPlacesList();
         buildQuery();
         log.trace("buildResponse -> {}", this);
     }
@@ -36,12 +35,12 @@ public class FindRequest extends Request {
         Database db = new Database();
 
         try {
-            this.placesList = db.places(this.match, this.limit);
+            this.places = db.places(this.match, this.limit);
             this.found = db.found(this.match);
         } catch (Exception e) {
             System.err.println("Caught Error from Database: " + e);
         }
     }
 
-    public Integer getPlacesListSize() { return this.placesList.size(); }
+    public Integer getPlacesSize() { return this.places.size(); }
 }
