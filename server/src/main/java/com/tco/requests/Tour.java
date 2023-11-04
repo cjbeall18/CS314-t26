15256f3c72e2e4912d2d39dd911775a13113cd7b
Chris Beall <cjbeall18@hotmail.com>
2023-11-04 16:29:46 +0000

    initial commit of tour skeleton

---
+++ server/src/main/java/com/tco/requests/Tour.java
@@ -0,0 +1,18 @@
+package com.tco.requests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tour {
+    double[][] distances; 
+    int[] tour;
+    Places places;
+
+    public void shorter () {
+
+    }
+
+    public void improve () {
+
+    }
+}
