b48507f60d9885119d179b52302159fb6e75670f
Evan Loy <loy.evan@yahoo.com>
2023-11-05 20:15:31 -0700

    implementation of Tour.java.

--- server/src/main/java/com/tco/requests/Tour.java
+++ server/src/main/java/com/tco/requests/Tour.java
@@ -1,4 +1,6 @@
 package com.tco.requests;
+import com.tco.misc.DistanceCalculator;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,15 +10,80 @@ public abstract class Tour {
     int[] tour;
     Places places;
 
-    public Tour () {
+    // Method to find the shortest tour using the nearest neighbor heuristic
+    public Places shorter(Places places, double earthRadius) {
+        Places bestTour = null;
+        long bestDistance = Long.MAX_VALUE;
 
+        // Calculate the distance between all pairs of cities once, to avoid recalculating
+        for (int i = 0; i < places.size(); i++) {
+            for (int j = 0; j < places.size(); j++) {
+                distances[i][j] = DistanceCalculator.calculator(places.get(i), places.get(j), earthRadius);
+            }
+        }
+
+        // Try constructing a tour starting from each city
+        for (int i = 0; i < places.size(); i++) {
+            Places currentTour = construct(i, places, earthRadius);
+            long currentDistance = calculateTourDistance(currentTour, earthRadius);
+
+            if (currentDistance < bestDistance) {
+                bestDistance = currentDistance;
+                bestTour = currentTour;
+            }
+        }
+
+        return bestTour;
     }
+    
+    // Helper method to construct a tour starting from a given city index
+    private Places construct(int startIndex, Places places, double earthRadius) {
+        Places tour = new Places();
+        boolean[] visited = new boolean[places.size()];
+        Arrays.fill(visited, false);
+        visited[startIndex] = true;
+        tour.add(places.get(startIndex));
 
-    public void shorter () {
+        int currentCityIndex = startIndex;
+        while (tour.size() < places.size()) {
+            int nearestCityIndex = -1;
+            double nearestDistance = Double.MAX_VALUE;
 
+            // Find the nearest unvisited city
+            for (int i = 0; i < places.size(); i++) {
+                if (!visited[i]) {
+                    double distance = distances[currentCityIndex][i];
+                    if (distance < nearestDistance) {
+                        nearestDistance = distance;
+                        nearestCityIndex = i;
+                    }
+                }
+            }
+
+            if (nearestCityIndex == -1) {
+                break; // This shouldn't happen if the logic is correct
+            }
+
+            // Mark the nearest city as visited and add it to the tour
+            visited[nearestCityIndex] = true;
+            tour.add(places.get(nearestCityIndex));
+            currentCityIndex = nearestCityIndex;
+        }
+
+        return tour;
     }
-    public void construct () {
-        
+    
+    // Helper method to calculate the total distance of a tour
+    private long calculateTourDistance(Places tour, double earthRadius) {
+        long totalDistance = 0;
+
+        for (int i = 0; i < tour.size(); i++) {
+            Place from = tour.get(i);
+            Place to = (i + 1 < tour.size()) ? tour.get(i + 1) : tour.get(0); // loop back to the start
+            totalDistance += DistanceCalculator.calculator(from, to, earthRadius);
+        }
+
+        return totalDistance;
     }
 
     abstract void improve ();
