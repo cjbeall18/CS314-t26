375e5739a3d586831ece31de56994b53bfd196e2
Ethan Pitera <ejpitera@gmail.com>
2023-11-07 17:54:01 -0700

    added shorter method to tour.java

--- server/src/main/java/com/tco/requests/Tour.java
+++ server/src/main/java/com/tco/requests/Tour.java
@@ -12,8 +12,36 @@ public abstract class Tour {
     int[] tour;
     Places places;
 
-    public void shorter () {
+    public Places shorter (Places places, double earthRadius) {
+        Places bestTour = null;
+        long bestDistance = Long.MAX_VALUE;
 
+        // Calculate the distance between all pairs of cities once, to avoid recalculating
+        distances = new double[places.size()][places.size()];
+        for (int i = 0; i < places.size(); i++) {
+            for (int j = 0; j < places.size(); j++) {
+                distances[i][j] = DistanceCalculator.calculator(places.get(i), places.get(j), earthRadius);
+            }
+        }
+
+        // Try constructing a tour starting from each city
+        for (int i = 0; i < places.size(); i++) {
+            Places currentTour = construct(i, places, earthRadius);
+            long currentDistance = calculateTourDistance(currentTour, earthRadius);
+
+            if (currentDistance < bestDistance) {
+                bestDistance = currentDistance;
+                bestTour = currentTour;
+            }
+        }
+
+        // Need if block to rotate bestTour to make first place same as first place in given places arraylist from user
+        if (places.get(0) != bestTour.get(0)) {
+            int firstPlaceIndex = bestTour.indexOf(places.get(0));
+            firstPlaceIndex *= -1;
+            Collections.rotate(bestTour, firstPlaceIndex);
+        }
+        return bestTour;
     }
     
     private Places construct (int startIndex, Places places, double earthRadius) {
