c59ff3045528e2064919038a49ed08dfba22e32b
Chris Beall <cjbeall18@hotmail.com>
2023-11-20 20:44:02 +0000

    added implementation for twoOptImproves

--- server/src/main/java/com/tco/requests/TwoOpt.java
+++ server/src/main/java/com/tco/requests/TwoOpt.java
@@ -2,15 +2,22 @@ package com.tco.requests;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.tco.misc.DistanceCalculator;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class TwoOpt extends Tour {
     void twoOpt() {}
 
     void improve() {}
 
-    private boolean twoOptImproves() {
-        return false;
+    private boolean twoOptImproves(Places route, int i, int k) {
+        return DistanceCalculator.calculator(route.get(i), route.get(k), 3695.0) + 
+        DistanceCalculator.calculator(route.get(i+1), route.get(k+1), 3695.0) < 
+        DistanceCalculator.calculator(route.get(i), route.get(i+1), 3695.0) + 
+        DistanceCalculator.calculator(route.get(k), route.get(k+1), 3695.0);
     }
 
     private void twoOptReverse() {}
+
 }
