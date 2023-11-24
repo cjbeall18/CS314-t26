fbd37e2dacc01551ab7818d4c97fdbb36f7d672a
Ethan Pitera <ejpitera@gmail.com>
2023-11-24 13:46:47 -0700

    added threeopt reverse methods

--- server/src/main/java/com/tco/requests/ThreeOpt.java
+++ server/src/main/java/com/tco/requests/ThreeOpt.java
@@ -8,4 +8,10 @@ import java.util.Collections;
 
 public class ThreeOpt extends Tour {
     void improve() {}
+
+    private boolean threeOptReverseI1J(int reversals) { return (reversals & 0b001) > 0; }
+    private boolean threeOptReverseJ1K(int reversals) { return (reversals & 0b010) > 0; }
+    private boolean threeOptReverseI1K(int reversals) { return (reversals & 0b100) > 0; }
+
+    
 }
