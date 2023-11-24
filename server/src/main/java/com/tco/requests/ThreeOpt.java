8e417a885aed72f49647f0d545b3feebf4ed6a61
Evan Loy <loy.evan@yahoo.com>
2023-11-24 13:18:57 -0700

    Added skeleton for ThreeOpt class.

---
+++ server/src/main/java/com/tco/requests/ThreeOpt.java
@@ -0,0 +1,11 @@
+package com.tco.requests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.tco.misc.DistanceCalculator;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ThreeOpt extends Tour {
+    void improve() {}
+}
