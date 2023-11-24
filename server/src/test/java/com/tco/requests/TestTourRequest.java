0472c4fd9d68f5016d23be568189624495b9deb6
Benjamin Ayala <bigben.design@outlook.com>
2023-11-24 13:06:56 -0700

    Skeleton for TestTourRequest

---
+++ server/src/test/java/com/tco/requests/TestTourRequest.java
@@ -0,0 +1,20 @@
+package com.tco.requests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTourRequest {
+    private TourRequest tourReq;
+
+    @BeforeEach
+    public void createTourRequestForTestCases() {
+        tourReq = new TourRequest();
+        tourReq.buildResponse();
+    }
+
+
+}
