4137be676a855a130fd5fa96ef244f7826268d94
Clay Roberson <crobersoncollege@gmail.com>
2023-11-26 15:02:55 -0700

    implemented 3opt improve

--- server/src/main/java/com/tco/requests/ThreeOpt.java
+++ server/src/main/java/com/tco/requests/ThreeOpt.java
@@ -5,9 +5,42 @@ import org.slf4j.LoggerFactory;
 import com.tco.misc.DistanceCalculator;
 import java.util.Arrays;
 import java.util.Collections;
+import com.tco.requests.TwoOpt;
 
 public class ThreeOpt extends Tour {
-    void improve() {}
+    @Override
+    void improve() {
+        Places route = this.places;
+        boolean improvement = true;
+        route.add(route.get(0));
+        int routeSize = route.size();
+        while (improvement) {
+            improvement = false;
+            for (int i = 0; i <= routeSize-3; i++) {
+                for (int j = i+1; j < routeSize-2; j++) {
+                    for (int k = j+1; k <= routeSize-1; k++) {
+                        int reversals = threeOptReversals(route, i, j, k);
+                        if (threeOptReverseI1J(reversals)) {
+                            TwoOpt.twoOptReverse(route, i+1, j);
+                            reversals -= 1;
+                        }
+                        if (threeOptReverseJ1K(reversals)) {
+                            TwoOpt.twoOptReverse(route, j+1, k);
+                            reversals -= 2;
+                        }
+                        if (threeOptReverseI1K(reversals)) {
+                            TwoOpt.twoOptReverse(route, i+1, k);
+                            reversals -= 4;
+                        }
+                        if (reversals > 0) {
+                            improvement = true;
+                        }
+                    }
+                }
+            }
+        }
+        TwoOpt.rotateStart(route, this.places);
+    }
 
     private boolean threeOptReverseI1J(int reversals) { return (reversals & 0b001) > 0; }
     private boolean threeOptReverseJ1K(int reversals) { return (reversals & 0b010) > 0; }
