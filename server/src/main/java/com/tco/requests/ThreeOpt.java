2ec9761ec32b7d19c1bbf7bb3b41779752b37056
Ethan Pitera <ejpitera@gmail.com>
2023-11-26 14:34:12 -0700

    added 3 opt reversal method

--- server/src/main/java/com/tco/requests/ThreeOpt.java
+++ server/src/main/java/com/tco/requests/ThreeOpt.java
@@ -13,5 +13,27 @@ public class ThreeOpt extends Tour {
     private boolean threeOptReverseJ1K(int reversals) { return (reversals & 0b010) > 0; }
     private boolean threeOptReverseI1K(int reversals) { return (reversals & 0b100) > 0; }
 
-    
+    private int threeOptReversals(Places route, int i, int j, int k) {
+        int binVal = 0b000;
+        long i1_leg = DistanceCalculator.calculator(route.get(i), route.get(i+1), 3958.8);
+        long j1_leg = DistanceCalculator.calculator(route.get(i), route.get(j+1), 3958.8);
+        long j_leg = DistanceCalculator.calculator(route.get(i), route.get(j), 3958.8);
+        long k_leg = DistanceCalculator.calculator(route.get(i), route.get(k), 3958.8);
+        if (i1_leg < k_leg) {
+            binVal = binVal | 0b001;
+        } else {
+            binVal = binVal | 0b000;
+        }
+        if (j1_leg < k_leg) {
+            binVal = binVal | 0b010;
+        } else {
+            binVal = binVal | 0b000;
+        }
+        if (i1_leg < j_leg) {
+            binVal = binVal | 0b100;
+        } else {
+            binVal = binVal | 0b000;
+        }
+        return binVal;
+    }
 }
