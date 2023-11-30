12c5fffec031ced8093b281e523699ed5b6e1379
Clay Roberson <crobersoncollege@gmail.com>
2023-11-30 16:14:22 -0700

    initial refactoring commit

--- server/src/main/java/com/tco/requests/Tour.java
+++ server/src/main/java/com/tco/requests/Tour.java
@@ -12,26 +12,30 @@ public abstract class Tour {
     Places places;
 
     public Places shorter (Places places, double earthRadius, double response) {
-        Places bestTour = places;
-        long bestDistance = calculateTourDistance(places, earthRadius);
+        System.out.println("places.size: " + places.size());
+        Place[] places_arr = places.toArray(new Place[places.size()+1]);
+        places_arr[places_arr.length-1] = places_arr[0];
+        Place[] bestTour = places_arr;
+        long bestDistance = calculateTourDistance(places_arr, earthRadius);
         long startTime = System.currentTimeMillis();
 
-        distances = new double[places.size()][places.size()];
-        for (int i = 0; i < places.size(); i++) {
-            for (int j = 0; j < places.size(); j++) {
+        distances = new double[places_arr.length][places_arr.length];
+        for (int i = 0; i < places_arr.length; i++) {
+            for (int j = 0; j < places_arr.length; j++) {
                 if (timeCheck(startTime, response)) {break;}
-                distances[i][j] = DistanceCalculator.calculator(places.get(i), places.get(j), earthRadius);
+                distances[i][j] = DistanceCalculator.calculator(places_arr[i], places_arr[j], earthRadius);
             }
         }
-        for (int i = 0; i < places.size(); i++) {
+        for (int i = 0; i < places_arr.length; i++) {
             if (timeCheck(startTime, response)) {break;}
-            Places currentTour = construct(i, places, earthRadius);
-            if (places.size() <= 250) {
-                Tour twoOptTour = new TwoOpt();
-                twoOptTour.places = currentTour;
-                twoOptTour.improve();
-                currentTour = twoOptTour.places;
-            }
+            Place[] currentTour = construct(i, places_arr, earthRadius);
+            // if (places_arr.length <= 250) {
+            //     Tour twoOptTour = new TwoOpt();
+            //     twoOptTour.places = currentTour;
+            //     twoOptTour.improve();
+            //     currentTour = twoOptTour.places;
+            // }
+            System.out.println("currentTour.length in shorter: " + currentTour.length);
             long currentDistance = calculateTourDistance(currentTour, earthRadius);
 
             if (currentDistance < bestDistance) {
@@ -39,14 +43,15 @@ public abstract class Tour {
                 bestTour = currentTour;
             }
         }
+        Places bestTourList = new Places(bestTour);
         if (!places.isEmpty()) {
-            if (places.get(0) != bestTour.get(0)) {
-                int firstPlaceIndex = bestTour.indexOf(places.get(0));
+            if (places.get(0) != bestTourList.get(0)) {
+                int firstPlaceIndex = bestTourList.indexOf(places.get(0));
                 firstPlaceIndex *= -1;
-                Collections.rotate(bestTour, firstPlaceIndex);
+                Collections.rotate(bestTourList, firstPlaceIndex);
             }
         }
-        return bestTour;
+        return bestTourList;
     }
 
     private boolean timeCheck(long startTime, double endTime) {
@@ -60,19 +65,20 @@ public abstract class Tour {
         }
     }
     
-    private Places construct (int startIndex, Places places, double earthRadius) {
-        Places tour = new Places();
-        boolean[] visited = new boolean[places.size()];
+    private Place[] construct (int startIndex, Place[] places_arr, double earthRadius) {
+        Place[] tour = new Place[places_arr.length + 1];
+        boolean[] visited = new boolean[places_arr.length];
         Arrays.fill(visited, false);
         visited[startIndex] = true;
-        tour.add(places.get(startIndex));
+        tour[0]=places_arr[startIndex];
 
         int currentCityIndex = startIndex;
-        while (tour.size() < places.size()) {
+        int increment = 0;
+        while (tour.length < places_arr.length) {
             int nearestCityIndex = -1;
             double nearestDistance = Double.MAX_VALUE;
 
-            for (int i = 0; i < places.size(); i++) {
+            for (int i = 0; i < places_arr.length; i++) {
                 if (!visited[i]) {
                     double distance = distances[currentCityIndex][i];
                     if (distance < nearestDistance) {
@@ -85,18 +91,19 @@ public abstract class Tour {
                 break; 
             }
             visited[nearestCityIndex] = true;
-            tour.add(places.get(nearestCityIndex));
+            tour[increment++] = places_arr[nearestCityIndex];
             currentCityIndex = nearestCityIndex;
         }
         return tour;
     }
 
-    public long calculateTourDistance(Places tour, double earthRadius) {
+    public long calculateTourDistance(Place[] tour, double earthRadius) {
         long totalDistance = 0;
 
-        for (int i = 0; i < tour.size(); i++) {
-            Place from = tour.get(i);
-            Place to = (i + 1 < tour.size()) ? tour.get(i + 1) : tour.get(0); // loop back to the start
+        for (int i = 0; i < tour.length; i++) {
+            Place from = tour[i];
+            System.out.println("tour.length in calculate: " + tour.length);
+            Place to = (i + 1 < tour.length) ? tour[i + 1] : tour[0]; // loop back to the start
             totalDistance += DistanceCalculator.calculator(from, to, earthRadius);
         }
 
