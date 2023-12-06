0d506cc3148bf19921571b55b71af41e25d3dc85
Chris Beall <cjbeall18@hotmail.com>
2023-12-06 17:50:45 +0000

    added debugging print statements for infinite loops

--- server/src/main/java/com/tco/requests/TwoOpt.java
+++ server/src/main/java/com/tco/requests/TwoOpt.java
@@ -16,12 +16,18 @@ public class TwoOpt extends Tour {
         Place[] routeArray = this.globalPlaces;
         boolean improvement = true;
         distances_matrix = this.distances;
-        // System.out.println("DISTANCES: " + distances_matrix);
+        System.out.println("DISTANCES: " + distances_matrix);
 
         while (improvement) {
+            System.out.println("While loop condition: " + improvement);
             improvement = false;
             for (int i=0; i<=routeArray.length-3; i++) {
+                System.out.println("Outer for loop condition: " + (i<=routeArray.length-3));
+                System.out.println("routeArray.length: " + routeArray.length);
+                System.out.println("i: " + i);
                 for (int k=i+2; k<routeArray.length-1; k++) {
+                    System.out.println("Inner for loop condition: " + (k<routeArray.length-1));
+                    System.out.println("k: " + k);
                     if (twoOptImproves(/*routeArray,*/ i, k)) {
                         routeArray = twoOptReverse(routeArray, i+1, k);
                         improvement = true;
@@ -40,7 +46,11 @@ public class TwoOpt extends Tour {
     }
 
     private boolean twoOptImproves(/*Place[] routeArray,*/ int i, int k) {
+        //System.out.println("Distances less than " + distances_matrix[i][k] + distances_matrix[i+1][k+1]);
+        //System.out.println("Distances greater than " + distances_matrix[i][i+1] + distances_matrix[k][k+1]);
+        System.out.println("If Statement Condition:  " + (distances_matrix[i][k] + distances_matrix[i+1][k+1] < distances_matrix[i][i+1] + distances_matrix[k][k+1]));
         return distances_matrix[i][k] + distances_matrix[i+1][k+1] < distances_matrix[i][i+1] + distances_matrix[k][k+1];
+
         // return DistanceCalculator.calculator(routeArray[i], routeArray[k], 3695.0) + 
         // DistanceCalculator.calculator(routeArray[i+1], routeArray[k+1], 3695.0) < 
         // DistanceCalculator.calculator(routeArray[i], routeArray[i+1], 3695.0) + 
